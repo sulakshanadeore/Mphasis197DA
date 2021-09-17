88ff435bcb1af51aabff2cb7e278ed4e364dd7b7
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-17 12:30:33 +0530

    Add files via upload

---
+++ Joins.sql
@@ -0,0 +1,55 @@
+use northwind
+go
+
+select * from products
+
+select * from Categories
+
+--Equi join---(=)
+select productid,productname,p.CategoryID,unitprice,categoryname,Description
+from products p inner join Categories c
+on p.CategoryID=c.CategoryID
+
+--Non-Equi---(Other than =)
+select productid,productname,p.CategoryID,unitprice,categoryname,Description
+from products p inner join Categories c
+on p.CategoryID<c.CategoryID
+order by ProductID
+
+select categoryid from categories
+order by CategoryID
+
+select 79*13
+
+select productid,productname,unitprice,p.categoryid,categoryname,description
+from Products p cross join categories c
+
+----permutations and combinations of one row of one table with all rows of the other table---- Cross  (x)
+select productid,productname,unitprice,c.categoryid,categoryname,description
+from Products p cross join categories c
+where productid=2
+
+1pr----all possible cateogories--
+
+----outer joins
+
+select * from categories
+select distinct(categoryid) from Products
+
+select * from products
+
+
+select productid,productname,p.categoryid,categoryname 
+from products p left outer join Categories c
+on p.CategoryID=c.CategoryID
+
+
+---			LHS								RHS			
+select productid,productname,p.categoryid,categoryname 
+from Categories c  right outer join products p
+on p.CategoryID=c.CategoryID
+
+
+select productid,productname,p.categoryid,categoryname 
+from Categories c  full outer join products p
+on p.CategoryID=c.CategoryID
